25658b790544bb69989f60a2da55aa47fcc7835c
Ryan Nguyen <rynguyen@g.hmc.edu>
2022-10-17 13:36:42 -0700

    Fixed favicon.ico

--- wk1/pages/index.tsx
+++ wk1/pages/index.tsx
@@ -5,10 +5,6 @@ export default function Index() {
         <>
             <Head>
                 <title>Ryan's Website</title>
-                <link rel="apple-touch-icon" sizes="180x180" href="/public/favicon/apple-touch-icon.png"/>
-                <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon//favicon-32x32.png"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon//favicon-16x16.png"/>
-                <link rel="manifest" href="/site.webmanifest"/>
                 <meta name="description" content="Welcome to my portfolio"/>
             </Head>
             <div id="navbar">
